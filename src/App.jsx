155e31123bbd920b5c3d8874ccf3799e3a35c2b3
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-06-21 15:37:24 -0500

    Register page is working correctly, API creates a new user upon post request

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 
 import Login from './components/Login'
+import Registration from './components/Registration'
 
 function App() {
   return (
     <div>
       <Login />
+      <Registration />
     </div>
   );
 }
---
+++ src/components/Registration.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const initialCredentials = {
+  username: "",
+  password: "",
+  instructorCode: ''
+};
+
+function Registration() {
+  const [credentials, setCredentials] = useState(initialCredentials);
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log("You have registered", credentials);
+    axios
+      .post(
+        "https://fitnessapplambda5.herokuapp.com/api/auth/register",
+        credentials
+      )
+      .then((res) => {
+        console.log(res);
+        // localStorage.setItem("token", res.data.token);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <h3>Register</h3>
+      <form onSubmit={handleRegister}>
+        <input type="text" name="username" onChange={handleChange} />
+        <input type="password" name="password" onChange={handleChange} />
+        <input type="text" name="instructorCode" onChange={handleChange} />
+        <button>Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Registration;
