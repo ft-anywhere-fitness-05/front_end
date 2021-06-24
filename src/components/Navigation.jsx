434254e958deade95cb9e36afd21dbd8b52013a1
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-06-24 11:22:22 -0700

    Creates filterClasses function

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-import SearchBar from "./SearchBar";
-
 const Navigation = () => {
   const userType = useSelector((state) => state.user.userType);
 
@@ -14,14 +12,9 @@ const Navigation = () => {
 
   return (
     <div>
-      <div>
-        <Link to="/dashboard">Dashboard</Link>
-        {userType === "client" && <Link to="/reservations">My Classes</Link>}
-        <Link onClick={logout}>Logout</Link>
-      </div>
-      <div>
-        <SearchBar />
-      </div>
+      <Link to="/dashboard">Dashboard</Link>
+      {userType === "client" && <Link to="/reservations">My Classes</Link>}
+      <Link onClick={logout}>Logout</Link>
     </div>
   );
 };
